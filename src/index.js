94f8f0ceae6c7221dbd45f6520146ec4fa5e14d3
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2021-04-17 19:39:27 +0800

    feat: add User model to the admin panel

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 const AdminBro = require('admin-bro');
+const AdminBroMongoose = require('@admin-bro/mongoose');
+AdminBro.registerAdapter(AdminBroMongoose);
+
 const AdminBroExpress = require('@admin-bro/express');
 
 const express = require('express');
 const app = express();
 
-const adminBro = new AdminBro({
-  databases: [],
-  rootPath: '/admin',
-});
+const mongoose = require('mongoose');
+const User = require('./models/User');
 
-const router = AdminBroExpress.buildRouter(adminBro);
-app.use(adminBro.options.rootPath, router);
+mongoose
+  .connect(/* Өөрийнхөө хаягийг оруулаарай */ 'TODO', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((db) => {
+    const adminBro = new AdminBro({
+      databases: [db],
+      resources: [User],
+      rootPath: '/admin',
+    });
 
-app.listen(8080, () =>
-  console.log('🚀 Server is running on http://localhost:8080'),
-);
+    const router = AdminBroExpress.buildRouter(adminBro);
+    app.use(adminBro.options.rootPath, router);
+
+    app.listen(8080, () =>
+      console.log('🚀 Server is running on http://localhost:8080'),
+    );
+  });
